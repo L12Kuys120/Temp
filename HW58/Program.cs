c67bdb37d53a0521dd654c82413fe9bc5462f852
Mikhail <radionetcom@inbox.ru>
2023-06-16 07:39:59 +0300

    Add change in foldes Temp

--- HW58/Program.cs
+++ HW58/Program.cs
@@ -5,3 +5,49 @@
 Результирующая матрица будет:
 18 20
 15 18*/
+
+using System;
+using static System.Console;
+
+int[,] array1 = new int[,] { { 2, 4 }, { 3, 2 }};
+int[,] array2 = new int[,] { { 3, 4 }, { 3, 3 }};
+
+PrintArray(array1);
+WriteLine();
+PrintArray(array2);
+WriteLine();
+int[,] array3 = MultiMatrix(array1, array2);
+PrintArray(array3);
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j],2} ");
+        }
+        WriteLine();
+    }
+}
+
+int[,] MultiMatrix(int[,] arr1, int[,] arr2)
+{
+    int n = 2; 
+    int[,] arr3 = new int[2, 2];
+   
+    for (int k = 0; k < n; k++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                arr3[i, j] += arr1[i, k] * arr2[k, j];
+             
+                
+            }   
+        }
+        
+    }return arr3;
+}
+
--- HW60/Program.cs
+++ HW60/Program.cs
@@ -5,3 +5,44 @@
 34(1,0,0) 41(1,1,0)
 27(0,0,1) 90(0,1,1)
 26(1,0,1) 55(1,1,1)*/
+
+using System;
+using static System.Console;
+
+int[, ,] array = GetArray(2, 2, 2, 10, 100);
+//GetArray(array);
+PrintArray(array);
+
+
+int[, ,] GetArray(int m, int n, int k, int min, int max)
+{
+    int[, ,] result = new int[m, n, k];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+           for (int l = 0; l < k; l++)
+           {
+            result[i, j, l] = new Random().Next(min, max + 1);
+           } 
+        }
+
+    }
+    return result;
+}
+
+void PrintArray(int[, ,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+           for (int l = 0; l < inArray.GetLength(2); l++)
+           {
+                Write($"{inArray[i, j, l],5} ({i}, {j}, {l})");
+                 
+           }
+            WriteLine();  
+        }  
+    }
+}   
